f31a9644ee461a2f4acb93ab71b89589fbca63f8
Vitor <vitorlopesdesouza@gmail.com>
2018-08-22 13:38:57 -0300

    update: index.html and main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 (function() {
   'use strict';
 
+  function checkStateLogin() {
+    FB.getLoginStatus(function(response) {
+      if (response.status === 'connected') {
+        // usuario logado no facebook e com o app aceito
+      } else if (response.status === 'not_authorized') {
+        // Usuario logado no facebook, mas nao aceitou o App
+      } else {
+        // Usuario nao logado no facebook
+      }
+    });
+  }
 
-
+  checkStateLogin();
 })();
