4638d9689c9522012c007fff1cfb113c12f0e58e
Vitor <vitorlopesdesouza@gmail.com>
2018-08-23 11:05:25 -0300

    update login

--- js/main.js
+++ js/main.js
@@ -2,9 +2,26 @@
   'use strict';
 
   var fbLogout = doc.querySelector('.fb-logout-button');
+  var fbLogin = doc.querySelector('.fb-login-button');
+  var tel = doc.querySelector('[data-js="tel"]');
+  var email = doc.querySelector('[data-js="email"]');
 
   fbLogout.addEventListener('click', function() {
     logout();
+  });
+
+  fbLogin.addEventListener('click', function() {
+    getInputValues();
+  });
+
+  var telefoneValue = '';
+  tel.addEventListener('input', function() {
+    telefoneValue = this.value;
+  });
+
+  var emailValue = '';
+  email.addEventListener('input', function() {
+    emailValue = this.value;
   });
 
   function logout() {
@@ -13,5 +30,9 @@
       console.log(response);
       console.log(JSON.stringify(response));
     });
+  }
+
+  function getInputValues() {
+    console.log(telefoneValue, emailValue);
   }
 })(window, document);
