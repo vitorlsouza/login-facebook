6fa07f8e3842a44f9fd373eec1133dcada77a7f8
Vitor <vitorlopesdesouza@gmail.com>
2018-08-23 00:27:17 -0300

    update

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@
       if (response.status === 'connected') {
         // usuario logado no facebook e com o app aceito
         console.log('teste1');
-        console.log(response);
+        console.log(response.authResponse.accessToken);
       } else if (response.status === 'not_authorized') {
         console.log('teste2');
-        console.log(response);
+        console.log(response.authResponse.accessToken);
         // Usuario logado no facebook, mas nao aceitou o App
       } else {
         console.log('teste3');
-        console.log(response);
+        console.log(response.authResponse.accessToken);
         // Usuario nao logado no facebook
       }
     });
