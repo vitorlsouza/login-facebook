e205430ea471fb246e3905f4c8f608d76e492c92
Vitor <vitorlopesdesouza@gmail.com>
2018-08-23 14:17:21 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
 (function(win, doc) {
   'use strict';
 
-  var fbLoginButton = doc.querySelector('.fb-login-button');
-  var tel = doc.querySelector('[data-js="tel"]');
-  var email = doc.querySelector('[data-js="email"]');
-
-  fbLoginButton.addEventListener('click', function() {
-    getInputValues();
-  });
-
-  var telefoneValue = '';
-  tel.addEventListener('input', function() {
-    telefoneValue = this.value;
-  });
-
-  var emailValue = '';
-  email.addEventListener('input', function() {
-    emailValue = this.value;
-  });
-
-  function getInputValues() {
-    alert(telefoneValue, emailValue);
-  }
 })(window, document);
