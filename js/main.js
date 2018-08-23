91b4f4a7173c94dbc32ec14c8887ef1a6e05a925
Vitor <vitorlopesdesouza@gmail.com>
2018-08-23 00:04:41 -0300

    update: buton facebook

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-(function() {
+(function(win, doc) {
   'use strict';
 
-})();
+  var fbLogin = doc.querySelector('.fb-login-button');
+
+  fbLogin.addEventListener('click', () => {
+    checkLoginState();
+  })
+
+  function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+      if (response.status === 'connected') {
+        // usuario logado no facebook e com o app aceito
+      } else if (response.status === 'not_authorized') {
+        // Usuario logado no facebook, mas nao aceitou o App
+      } else {
+        // Usuario nao logado no facebook
+      }
+    });
+  }
+
+})(window, document);
